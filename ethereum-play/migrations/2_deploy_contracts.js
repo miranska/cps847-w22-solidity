7ea3c16e5cbe60b29793fc9e64df2c62c36083e5
Andriy Miranskyy <avm@ryerson.ca>
2022-04-03 12:25:27 -0400

    Add deploy instructions for the proof of existence contract

---
+++ ethereum-play/migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+// the file is created manually
+var ProofOfExistence1 = artifacts.require("./ProofOfExistence1.sol");
+module.exports = function(deployer) {
+  deployer.deploy(ProofOfExistence1);
+};
