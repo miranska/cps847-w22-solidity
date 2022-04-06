8108801463a6e88151ea6b7ff5b01790b34670a4
Andriy Miranskyy <avm@ryerson.ca>
2022-04-05 20:20:48 -0400

    Fix typo in code

--- ethereum-play/migrations/2_deploy_contracts.js
+++ ethereum-play/migrations/2_deploy_contracts.js
@@ -2,6 +2,6 @@
 var ProofOfExistence1 = artifacts.require("./ProofOfExistence1.sol");
 var ProofOfExistence2 = artifacts.require("./ProofOfExistence2.sol");
 module.exports = function(deployer) {
-  deployer.deploy(ProofOfExistence1, {gas: 471200}));
-  deployer.deploy(ProofOfExistence2, {gas: 471200}));
+  deployer.deploy(ProofOfExistence1, {gas: 471200});
+  deployer.deploy(ProofOfExistence2, {gas: 471200});
 };
